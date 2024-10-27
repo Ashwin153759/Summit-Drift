046446fe05371ed3c77225fd8f17019e587e0f46
Ashwin Parapuram <ashwinpara137@gmail.com>
2024-10-27 15:41:55 -0400

    spectator cheering logic

---
+++ Assets/Scripts/Animations/RandomCheerAndCheerDistance.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCheerAndCheerDistance : MonoBehaviour
+{
+
+    [SerializeField] private GameObject car;
+
+    private float cheerDistance = 10f;
+    private Animator animator;
+
+    void Start() {
+        animator = GetComponent<Animator>(); // Get the Animator component
+
+        if (car == null) {
+            car = GameObject.FindWithTag("Player");
+        }
+
+        animator.SetBool("IsIdle", true);
+    }
+
+    void Update() {
+
+        //current distance between car and spectator
+        float currentDistance = Vector3.Distance(transform.position, car.transform.position);
+
+        //if the spectator is within cheerDistance of the car, then a random cheer is assigned to them
+        if (currentDistance <= cheerDistance) {
+            if (animator != null) {
+                animator.SetBool("IsIdle", false);
+                AssignRandomCheeringAnimation();
+            }
+        } 
+        else {
+            animator.SetBool("IsIdle", true);
+        }
+
+
+    }
+
+    private void AssignRandomCheeringAnimation() {
+        if (animator == null) {
+            Debug.LogWarning($"Animator not found on {gameObject.name}");
+            return;
+        }
+
+        // Generate a random integer based on the number of cheering animations (0, 1, 2, 3)
+        int randomCheer = Random.Range(0, 4);
+
+        // Set the integer parameter in the Animator to trigger the appropriate animation
+        animator.SetInteger("CheerIndex", randomCheer);
+
+        // Debug.Log($"{gameObject.name} is playing animation with CheerIndex: {randomCheer}");
+    }
+}
