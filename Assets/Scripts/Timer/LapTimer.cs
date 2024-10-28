3adb332bae8d245f362bb0fc915abdc03be50b3e
Michael <micraidz@gmail.com>
2024-10-28 04:06:01 -0400

    Simple Game Manager - Needs a lot of tweaking

--- Assets/Scripts/Timer/LapTimer.cs
+++ Assets/Scripts/Timer/LapTimer.cs
@@ -7,6 +7,16 @@ public class LapTimer : MonoBehaviour
     public bool lapActive = true;
     public float lapTime = 0f;
 
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        // Find the GameManager instance
+        gameManager = FindObjectOfType<GameManager>();
+        if (gameManager != null)
+            gameManager.StartRace();
+    }
+
     private void Update()
     {
         if (lapActive)
@@ -22,15 +32,20 @@ public class LapTimer : MonoBehaviour
         lapTimeText.text = "Lap Time: " + lapTime.ToString("F3");
     }
 
-    // Called by SectorManager at lap end
     public void ResetLap()
     {
         lapTime = 0f;
         lapActive = true;
+        
+        if (gameManager != null)
+            gameManager.StartRace();
     }
 
     public void StopLap()
     {
         lapActive = false;
+        
+        if (gameManager != null)
+            gameManager.EndRace();
     }
 }
